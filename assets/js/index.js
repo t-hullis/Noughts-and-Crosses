75daadcd3d830fd70fc99a4c1e55d5793834065f
t-hullis <t.hullis@yahoo.com>
2022-07-01 19:07:01 +0000

    add players to javascript and add winning ccombos

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,13 @@
-console.log('hi');
+let board;
+let player1;
+let player2;
+let winners = [
+    [0,1,2]
+    [3,4,5]
+    [6,7,8]
+    [0,4,8]
+    [2,4,6]
+    [0,3,6]
+    [1,4,7]
+    [2,5,8]
+]
